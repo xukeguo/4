cba56729594d3c8d3e2ad8a743ceeb5cd8209ea8
xukeguo <108842875@qq.com>
2022-04-22 18:31:00 +0800

    t
    t

--- 1/test.py
+++ 1/test.py
@@ -1,19 +1,15 @@
-a=45454
-b=str(a)
-a1,a2,a3,a4,a5=[*b]
-print(a1,a2,a3,a4,a5,sep="*")#sep可以设置分隔符
-print(a1,a2,a3,a4,a5,end="\n")#end可以设置结束符
-c=a1*2
+a = 45454
+b = str(a)
+a1, a2, a3, a4, a5 = [ * b]
+print(a1, a2, a3, a4, a5, sep = "*") #sep可以设置分隔符
+print(a1, a2, a3, a4, a5, end = "\n") #end可以设置结束符
+c = a1 * 2
 print(c)
-a=45454#获取个位数。。。。。。
-b=str(a)
-a1,a2,a3,a4,a5=[int(*i) for i in b]
-c=a1**2
+a = 45454 #获取个位数。。。。。。
+b = str(a)
+a1, a2, a3, a4, a5 = [int( * i) for i in b]#我的统一转换成int赋值
+c = a1 ** 2
 print(c)
-print(a1,a2,a3,a4,a5,sep="*")#sep可以设置分隔符
-print(a1,a2,a3,a4,a5,end="\n")#end可以设置结束符
-#工作簿
-import 
-#创建工作簿
-wb=xlwt.Workbook()
-music.mus
+print(a1, a2, a3, a4, a5, sep = "*") #sep可以设置分隔符
+print(a1, a2, a3, a4, a5, end = "\n") #end可以设置结束符
+
--- mypapeage/用法文档.py
+++ mypapeage/用法文档.py
@@ -1377,7 +1377,20 @@ with open ('/Volumes/系统二/F-drive-214171/music2/陈慧娴 - 千千阙歌.fl
     target_file.write (src_file.read())
  
 
-
+a = 45454
+b = str(a)
+a1, a2, a3, a4, a5 = [ * b]
+print(a1, a2, a3, a4, a5, sep = "*") #sep可以设置分隔符
+print(a1, a2, a3, a4, a5, end = "\n") #end可以设置结束符
+c = a1 * 2
+print(c)
+a = 45454 #获取个位数。。。。。。
+b = str(a)
+a1, a2, a3, a4, a5 = [int( * i) for i in b]#我的统一转换成int赋值
+c = a1 ** 2
+print(c)
+print(a1, a2, a3, a4, a5, sep = "*") #sep可以设置分隔符
+print(a1, a2, a3, a4, a5, end = "\n") #end可以设置结束符
 
 
 
